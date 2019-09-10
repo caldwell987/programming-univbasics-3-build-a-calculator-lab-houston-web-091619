505de73d13d348849e566b83d1badd76d7058843
David Caldwell <david@avvitt.com>
2019-09-10 23:04:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,22 +14,23 @@ puts two
 def division(num1=5, num2=4)
   return num1/num2
 end
-three = subtraction
+three = division
 puts three
 
 
 def multiplication(num1=5, num2=4)
   return num1*num2
 end
-four = subtraction
+four = multiplication
 puts four
 
 
 
 def modulo(num1=5, num2=4)
-
+  return num1/num2
 end
-
+five = modulo
+puts five
 
 
 
